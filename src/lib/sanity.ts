541d2938ecdcf0760009c9c4b9e5e3fdbc7ca194
Zak Mayfield <zakmayfield@gmail.com>
2023-08-04 17:51:17 -0400

    init sanity client with basic schema

---
+++ src/lib/sanity.ts
@@ -0,0 +1,14 @@
+import { createClient } from '@sanity/client';
+
+import { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } from '$env/static/public';
+
+if (!PUBLIC_SANITY_PROJECT_ID || !PUBLIC_SANITY_DATASET) {
+	throw new Error('Please include projet ID and dataset.');
+}
+
+export const client = createClient({
+	projectId: PUBLIC_SANITY_PROJECT_ID,
+	dataset: PUBLIC_SANITY_DATASET,
+	apiVersion: '2023-08-04',
+	useCdn: false
+});

9279305a4f9ad133fe3a47c2e26556f4ee489758
Zak Mayfield <zakmayfield@gmail.com>
2023-08-07 15:01:36 -0400

    footer

--- src/features/footer/Footer.svelte
+++ src/features/footer/Footer.svelte
@@ -1,20 +1,11 @@
+<script>
+	import Logo from '../../shared/components/Logo.svelte';
+	import Socials from '../../shared/components/Socials.svelte';
+	import NavLinks from '../../shared/components/NavLinks.svelte';
+</script>
+
 <footer>
-	<!-- logo -->
-	<div>
-		<p>ZM</p>
-	</div>
-
-	<!-- social links -->
-	<div>
-		<a href="#">GH</a>
-		<a href="#">IN</a>
-	</div>
-
-	<!-- nav items -->
-	<div>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/experience">Experience</a></li>
-		</ul>
-	</div>
+	<Logo />
+	<Socials />
+	<NavLinks />
 </footer>

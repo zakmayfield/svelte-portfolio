3c044e4092843f82953d171782936562cb091ea4
Zak Mayfield <zakmayfield@gmail.com>
2023-08-08 16:30:50 -0400

    social link styles

--- src/shared/components/Socials.svelte
+++ src/shared/components/Socials.svelte
@@ -1,4 +1,12 @@
-<div>
-	<a href="https://github.com/zakmayfield">GH</a>
-	<a href="https://linkedin.com/in/zacharymayfield">IN</a>
+<div class="flex items-center gap-6">
+	<a
+		href="https://github.com/zakmayfield"
+		class="p-3 w-12 h-12 rounded-md bg-slate-100 hover:bg-slate-200 transition-colors flex justify-center items-center"
+		>GH</a
+	>
+	<a
+		href="https://linkedin.com/in/zacharymayfield"
+		class="p-3 rounded-md w-12 h-12 bg-slate-100 hover:bg-slate-200 transition-colors flex justify-center items-center"
+		>IN</a
+	>
 </div>

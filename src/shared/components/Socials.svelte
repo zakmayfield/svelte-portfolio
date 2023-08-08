55584033bf7ed11cc7719c0c096bf439c70bf8ef
Zak Mayfield <zakmayfield@gmail.com>
2023-08-08 16:47:37 -0400

    imported and used tabler icons

--- src/shared/components/Socials.svelte
+++ src/shared/components/Socials.svelte
@@ -1,12 +1,16 @@
+<script lang="ts">
+	import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-svelte';
+</script>
+
 <div class="flex items-center gap-6">
 	<a
 		href="https://github.com/zakmayfield"
 		class="p-3 w-12 h-12 rounded-md bg-slate-100 hover:bg-slate-200 transition-colors flex justify-center items-center"
-		>GH</a
+		><IconBrandGithub size={24} /></a
 	>
 	<a
 		href="https://linkedin.com/in/zacharymayfield"
 		class="p-3 rounded-md w-12 h-12 bg-slate-100 hover:bg-slate-200 transition-colors flex justify-center items-center"
-		>IN</a
+		><IconBrandLinkedin size={24} /></a
 	>
 </div>

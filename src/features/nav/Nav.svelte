3e1a387783f2996b728d50e06bdd70ac28e5d306
Zak Mayfield <zakmayfield@gmail.com>
2023-08-08 16:13:56 -0400

    using dynamic container

--- src/features/nav/Nav.svelte
+++ src/features/nav/Nav.svelte
@@ -2,10 +2,13 @@
 	import Logo from '../../shared/components/Logo.svelte';
 	import Socials from '../../shared/components/Socials.svelte';
 	import NavLinks from '../../shared/components/NavLinks.svelte';
+	import Container from '../../shared/components/Container.svelte';
 </script>
 
-<nav class="flex justify-between items-center">
-	<Socials />
-	<Logo />
-	<NavLinks />
+<nav class="">
+	<Container classes="max-w-xs  flex justify-between items-center">
+		<Socials />
+		<Logo />
+		<NavLinks />
+	</Container>
 </nav>
---
+++ src/shared/components/Container.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { merge } from '$lib/utils';
+	export let classes = '';
+</script>
+
+<div class={`${merge('border mx-auto max-w-2xl ', classes)}`}>
+	<slot />
+</div>

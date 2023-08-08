bdca7f47522d07137d1063b2a58b8dec2e1c7931
Zak Mayfield <zakmayfield@gmail.com>
2023-08-08 16:54:35 -0400

    add container to footer, adjust footer links

--- src/features/footer/Footer.svelte
+++ src/features/footer/Footer.svelte
@@ -2,10 +2,13 @@
 	import Logo from '../../shared/components/Logo.svelte';
 	import Socials from '../../shared/components/Socials.svelte';
 	import NavLinks from '../../shared/components/NavLinks.svelte';
+	import Container from '../../shared/components/Container.svelte';
 </script>
 
 <footer>
-	<Logo />
-	<Socials />
-	<NavLinks />
+	<Container classes="flex justify-between items-center">
+		<Logo />
+		<Socials />
+		<NavLinks classes="flex-col items-end" />
+	</Container>
 </footer>

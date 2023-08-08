fff4176557e1fb2a0f3f055f76c6c68251600d5f
Zak Mayfield <zakmayfield@gmail.com>
2023-08-08 16:55:26 -0400

    clean up navlinks, dynamic classes

--- src/shared/components/NavLinks.svelte
+++ src/shared/components/NavLinks.svelte
@@ -1,6 +1,9 @@
-<div>
-	<ul>
-		<li><a href="/">Home</a></li>
-		<li><a href="/experience">Experience</a></li>
-	</ul>
-</div>
+<script>
+	import { merge } from '$lib/utils';
+	export let classes = '';
+</script>
+
+<ul class={`${merge('flex justify-between items-center', classes)}`}>
+	<li><a href="/">Home</a></li>
+	<li><a href="/experience">Experience</a></li>
+</ul>

539fde693676646af09cc8bbac7d3daa5cbdcc15
Zak Mayfield <zakmayfield@gmail.com>
2023-08-09 13:58:20 -0400

    added sanity image src builder

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { goto } from '$app/navigation';
+import imageUrlBuilder from '@sanity/image-url';
+import type { Image } from '@sanity/types';
+import { client } from '$lib/sanity';
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: Image) {
+	return builder.image(source);
+}
 
 export function merge(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));

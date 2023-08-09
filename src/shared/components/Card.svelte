2eb5aecfa65cab0efb7a4a0be6e0f9635053d926
Zak Mayfield <zakmayfield@gmail.com>
2023-08-09 13:20:12 -0400

    added card content

--- src/shared/components/Card.svelte
+++ src/shared/components/Card.svelte
@@ -16,4 +16,17 @@
 	class="cursor-pointer"
 >
 	<h2>{exp.company}</h2>
+
+	<p>{exp.workTitle}</p>
+	<ul class="flex gap-6">
+		{#each exp.tags as tag}
+			<li>{tag}</li>
+		{/each}
+	</ul>
+
+	<h3>Description</h3>
+	<p>{exp.description}</p>
+
+	<h3>Challenges</h3>
+	<p>{exp.challenges}</p>
 </div>

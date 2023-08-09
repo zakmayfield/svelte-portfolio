2d6a633fba5d802ee8589717085ea984b4e4df95
Zak Mayfield <zakmayfield@gmail.com>
2023-08-09 11:35:25 -0400

    created navigate util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { goto } from '$app/navigation';
 
 export function merge(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function navigate(route: string) {
+	goto(route);
 }

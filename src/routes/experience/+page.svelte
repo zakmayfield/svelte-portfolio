d51c3963e08c2eab9cce73bb2aa229ad045f3c68
Zak Mayfield <zakmayfield@gmail.com>
2023-08-09 15:15:10 -0400

    cards layout name

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Card from '../../shared/components/Card.svelte';
-	import CardLayout from '../../features/cards/CardLayout.svelte';
+	import CardsLayout from '../../features/cards/CardsLayout.svelte';
 	import Container from '../../shared/components/Container.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
@@ -9,11 +9,11 @@
 <Container classes="max-w-3xl">
 	<h1 class="font-semibold text-lg mb-6">Experience</h1>
 
-	<CardLayout>
+	<CardsLayout>
 		{#if data.experience.length}
 			{#each data.experience as exp}
 				<Card {exp} />
 			{/each}
 		{/if}
-	</CardLayout>
+	</CardsLayout>
 </Container>

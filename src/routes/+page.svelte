afb0e2afeb36079fe0d576d62a6ea094feacf5f9
Zak Mayfield <zakmayfield@gmail.com>
2023-08-10 19:45:57 -0400

    chore: hover margins and width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,12 +11,12 @@
 
 <!-- hero -->
 <div
-	class="h-[80vh] bg-gradient-to-b from-orange-600 to-white flex items-center relative overflow-hidden"
+	class="h-[80vh] mb-24 bg-gradient-to-b from-orange-600 to-white flex items-center relative overflow-hidden"
 />
 
 <!-- intro -->
 <section>
-	<Container classes="max-w-2xl p-0">
+	<Container classes="max-w-3xl p-0">
 		<h3 class="text-xl mb-6">Thanks for stopping by</h3>
 
 		<Container classes="max-w-xl p-0 flex flex-col gap-6">
@@ -36,29 +36,34 @@
 </section>
 
 <!-- recent projects -->
-<section>
-	<Container classes="max-w-3xl grid grid-cols-2 justify-items-center">
-		{#if data.recentWork}
-			{#each data.recentWork as x}
-				<div
-					role="button"
-					on:click={() => handleClick(x)}
-					on:keypress={() => handleClick(x)}
-					tabindex="0"
-					class="relative flex justify-center items-center cursor-pointer shadow-md rounded-lg overflow-hidden w-[300px]"
-				>
-					<img
-						src={urlFor(x.image).width(300).height(150).url()}
-						alt="Cover image for {x.company}"
-						class="object-cover h-full object-center"
-					/>
-					<div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70">
-						<h4 class="text-white tracking-wide font-thin">
-							{x.company}
-						</h4>
+<section class="my-24">
+	<Container classes="max-w-3xl p-0">
+		<h3 class="text-xl mb-6">Recent work</h3>
+		<Container classes="max-w-3xl grid grid-cols-2 justify-items-center">
+			{#if data.recentWork}
+				{#each data.recentWork as x}
+					<div
+						role="button"
+						on:click={() => handleClick(x)}
+						on:keypress={() => handleClick(x)}
+						tabindex="0"
+						class="relative flex justify-center items-center cursor-pointer shadow-md rounded-lg overflow-hidden w-[300px]"
+					>
+						<img
+							src={urlFor(x.image).width(300).height(150).url()}
+							alt="Cover image for {x.company}"
+							class="object-cover h-full object-center"
+						/>
+						<div
+							class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 hover:bg-opacity-60"
+						>
+							<h4 class="text-white tracking-wide font-thin">
+								{x.company}
+							</h4>
+						</div>
 					</div>
-				</div>
-			{/each}
-		{/if}
+				{/each}
+			{/if}
+		</Container>
 	</Container>
 </section>

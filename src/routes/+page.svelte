170b079163e6584d5b1a892b53786929e34078f9
Zak Mayfield <zakmayfield@gmail.com>
2023-08-10 16:45:25 -0400

    feat:intro section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,31 @@
-<h1 class="text-3xl font-bold underline">Welcome</h1>
+<script>
+	import Container from '../shared/components/Container.svelte';
+</script>
+
+<!-- hero -->
+<div
+	class="h-[80vh] bg-gradient-to-b from-orange-600 to-white flex items-center relative overflow-hidden"
+/>
+
+<!-- intro -->
+<section>
+	<Container classes="max-w-2xl p-0">
+		<h3 class="text-xl mb-6">Thanks for stopping by</h3>
+
+		<Container classes="max-w-lg p-0 flex flex-col gap-6">
+			<p>
+				My name is Zachary, I am a US/Canada based front end TypeScript developer, with a knowledge
+				of Node.js backend architecture. I love building with the latest tech and Im constantly
+				learning.
+			</p>
+			<p>
+				I have worked on projects with tech such as TypeScript, React (Next.js/Remix), Express, and
+				CSS-in-JS libraries. Im familiar with RESTful & GraphQL server architecture & I prefer
+				working with relational databases like Postgres, I enjoy working with ORMs like Prisma to
+				help ease the pain of SQL.
+			</p>
+		</Container>
+	</Container>
+</section>
+
+<!-- recent projects -->

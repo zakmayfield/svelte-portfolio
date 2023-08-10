0832b535af3d356f30d3a113952b88bfed064cf8
Zak Mayfield <zakmayfield@gmail.com>
2023-08-10 17:53:01 -0400

    server call for recent work

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,23 @@
+import { client } from '$lib/sanity';
+import { error } from '@sveltejs/kit';
+
+export async function load() {
+	const recentWork = await client.fetch(`
+		*[_type == 'experience'][0...2]{
+			_id,
+			slug,
+			company,
+			image
+		}
+	`);
+
+	console.log(recentWork);
+
+	if (!recentWork) {
+		throw error(404);
+	}
+
+	return {
+		recentWork
+	};
+}
